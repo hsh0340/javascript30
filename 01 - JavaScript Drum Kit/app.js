2ebc05e67ce8fe2cd9f766031ada7b01e7e51870
hsh0340 <hsh0340@naver.com>
2022-09-13 22:21:24 +0900

    20220913

---
+++ 01 - JavaScript Drum Kit/app.js
@@ -0,0 +1,21 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  // console.log(audio);
+  if(!audio) return; // stop the function from running all together
+  audio.currentTime = 0;
+  audio.play();
+  console.log(key);
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return; // skip it if it's not a transform
+  // console.log(e.propertyName);
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
